c009a92ec2551ffc123fb3f656fe3a361e9211b9
JaaJPlayz <hiagoaugustofcunha@gmail.com>
2024-10-11 18:44:25 -0300

    Switching from firefox to google chrome

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,11 +4,17 @@ if status is-interactive
 
 	alias t="tmux"
 
+  alias v="nvim"
+  alias vi="nvim"
+  alias vim="nvim"
+
+  alias py="python3"
 	alias pyvenv="python3 -m venv .venv && source .venv/bin/activate.fish"
 	alias pyso="source .venv/bin/activate.fish"
 	alias pyfreeze="pip freeze > dev-requirements.txt"
+  alias pyinstall="pip install -r dev-requirements.txt"
 
-	alias fastfetch="fastfetch --logo zorin"
+	alias fastfetch="fastfetch --logo minix"
 
 	zoxide init fish | source
 	starship init fish | source
--- .config/nvim/lua/user/settings/init.lua
+++ .config/nvim/lua/user/settings/init.lua
@@ -4,10 +4,10 @@ vim.wo.relativenumber = true
 vim.wo.cursorline = true
 
 -- tabstop
-vim.bo.tabstop = 2
-vim.bo.softtabstop = 2
-vim.bo.shiftwidth = 2
-vim.bo.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
 
 -- indent
 vim.bo.autoindent = true
